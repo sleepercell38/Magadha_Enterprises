e875c01a85f3e29baccc3d88637e22b20fd8e944
sleepercell38 <insanevishal07@gmail.com>
2025-10-28 01:55:51 +0530

    api

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `${import.meta.env.VITE_API_URL}/api/admin`;
+const BASE_URL = `${import.meta.env.VITE_API_URL}/admin`;
 
 const api = axios.create({
   baseURL: BASE_URL,
--- frontend/src/api/eventApi.js
+++ frontend/src/api/eventApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${import.meta.env.VITE_API_URL}/api/projects`;
+const API_URL = `${import.meta.env.VITE_API_URL}projects`;
 
 export const getEventMetadataApi = async (token) => {
   const res = await axios.get(`${API_URL}/event-metadata`, {
